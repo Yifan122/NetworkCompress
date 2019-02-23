4e6beb3e64456b7121a9efce5926bd48bc9c234f
Yifan Zeng <zengyifan122@gmail.com>
2019-02-23 10:50:36 +0800

    update

--- pytorch/TaylorExpansionPrunner.py
+++ pytorch/TaylorExpansionPrunner.py
@@ -1,3 +1,4 @@
+import torch
 class TaylerExpansionPrunner:
     def __init__(self, model):
         self.model = model
@@ -5,6 +6,18 @@ class TaylerExpansionPrunner:
 
     def reset(self):
         self.filter_ranks = {}
+        self.reset_act_grad()
+        self.filter_ranks = {'layer1': [],
+                            'layer2': [],
+                            'layer3': [],
+                            'layer4': []}
+        for name in self.filter_ranks.keys():
+            layer = getattr(self.model, name)
+            for i in range(len(layer._modules)):
+                self.filter_ranks[name].append([])
+
+
+    def reset_act_grad(self):
         self.activations = {'layer1': [],
                             'layer2': [],
                             'layer3': [],
@@ -21,9 +34,44 @@ class TaylerExpansionPrunner:
                 self.activations[name].append([])
                 self.gradients[name].append([])
 
-    # def forward(self, x):
-    #     self.activations = []
-    #     self.gradients = []
+        for name in self.activations.keys():
+            layer = getattr(self.model, name)
+            for i in range(len(layer._modules)):
+                layer._modules[i].conv1.register_forward_hook(self.get_activation, self.activations[name][i])
+                layer._modules[i].conv1.register_hook(self.get_grad, self.gradients[name][i])
+
+    def get_activation(self, activation):
+        def hook(model, input, output):
+            activation = output.detach()
+        return hook
+
+    def get_grad(self, gradient):
+        def hook(grad):
+            gradient = grad
+        return hook
+
+    def forward(self, x):
+        self.reset_act_grad()
+        return self.model(x)
+
+    def compute_rank(self, grad):
+        activation_index = len(self.activations) - self.grad_index - 1
+        activation = self.activations[activation_index]
+        values = \
+            torch.sum((activation * grad), dim=0, keepdim=True). \
+                sum(dim=2, keepdim=True).sum(dim=3, keepdim=True)[0, :, 0, 0].data
+        # modified by
+
+        # Normalize the rank by the filter dimensions
+        values = \
+            values / (activation.size(0) * activation.size(2) * activation.size(3))
+
+        if activation_index not in self.filter_ranks:
+            self.filter_ranks[activation_index] = \
+                torch.FloatTensor(activation.size(1)).zero_().cuda()
+
+        self.filter_ranks[activation_index] += values
+        self.grad_index += 1
 
     def TaylorExpansion(self):
         print("TO Do")
